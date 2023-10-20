1d122861e6c9f895c39a21e2ecb3202ec5e3cb0d
Ravindu007 <ravindu0504@gmail.com>
2023-10-21 00:03:18 +0530

    integrate tailwind, bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          {/* Home Page Router */}
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {Brows} from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      
+    </div>
   )
 }
 
